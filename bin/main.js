baec3900316d50deaffe0832daa4acffe3af525c
Renan Porto <renan@vistasoft.com.br>
2020-01-30 14:10:17 -0300

    Adicionando opção para passar quantidade e moeda

--- bin/main.js
+++ bin/main.js
@@ -3,5 +3,8 @@
 
 var program = require('commander');
 var pkg = require('../package.json');
+var converterBTC = require('./ConvertBTC');
 
-program.version(pkg.version).description('Convert Bitcoin to any currency defined.').parse(process.argv);
+program.version(pkg.version).description('Convert Bitcoin to any currency defined.').option('-C, --currency <currency>', 'Currency to be converted. (Default: USD)').option('-A, --amount <amount>', 'Value in Bictoin to be convert. (Default: 1)').parse(process.argv);
+
+console.log(converterBTC(program.currency, program.amount));
