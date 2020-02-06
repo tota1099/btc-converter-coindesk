50ba67d7e7eb9a331b7a59665005be528b2349fd
Renan Porto <renan@vistasoft.com.br>
2020-02-06 09:24:58 -0300

    Ajustando avisos do lint

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,11 +1,13 @@
-const request = require("request");
+const request = require('request');
 
-function convertBTC(currency = "USD", amount = 1) {
+function convertBTC(currency = 'USD', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
 
   request(url, (error, response, body) => {
-    let apiResponse = JSON.parse(body);
+    const apiResponse = JSON.parse(body);
+    /* eslint-disable no-console */
     console.log(`${amount} BTC to ${currency} = ${apiResponse.price}`);
+    /* eslint-enable no-console */
   });
 }
 
--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ const pkg = require('../package.json');
 const converterBTC = require('./ConvertBTC');
 
 program
-    .version(pkg.version)
-    .description('Convert Bitcoin to any currency defined.')
-    .option('-C, --currency <currency>', 'Currency to be converted. (Default: USD)')
-    .option('-A, --amount <amount>', 'Value in Bictoin to be convert. (Default: 1)')
-    .parse(process.argv)
+  .version(pkg.version)
+  .description('Convert Bitcoin to any currency defined.')
+  .option(
+    '-C, --currency <currency>',
+    'Currency to be converted. (Default: USD)',
+  )
+  .option(
+    '-A, --amount <amount>',
+    'Value in Bictoin to be convert. (Default: 1)',
+  )
+  .parse(process.argv);
 
+/* eslint-disable no-console */
 console.log(converterBTC(program.currency, program.amount));
+/* eslint-enable no-console */
