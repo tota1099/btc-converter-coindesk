2fa84ec15aaab4e0b5be4e4263160fb186184ca1
Renan Porto <renan@vistasoft.com.br>
2020-02-06 09:20:44 -0300

    Ajustando formatação do código

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,12 +1,12 @@
-const request = require('request');
+const request = require("request");
 
-function convertBTC(currency = 'USD', amount = 1) {
-    const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+function convertBTC(currency = "USD", amount = 1) {
+  const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
 
-    request(url, (error, response, body) => {
-        let apiResponse = JSON.parse(body);
-        console.log(`${amount} BTC to ${currency} = ${apiResponse.price}`);
-    });
+  request(url, (error, response, body) => {
+    let apiResponse = JSON.parse(body);
+    console.log(`${amount} BTC to ${currency} = ${apiResponse.price}`);
+  });
 }
 
 module.exports = convertBTC;
