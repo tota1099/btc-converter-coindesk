5fc946e7900bc7c4c519d1ac13b9acbdd3ebfb9d
Renan Porto <renan@vistasoft.com.br>
2020-02-07 13:25:16 -0300

    Ajustando projeto para utilizar o async/await

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const chalk = require('chalk');
 const ora = require('ora');
-const request = require('request');
+const request = require('request-promise-native');
 
 const spinner = ora({
   text: 'Retrieving Bitcoin data...',
@@ -12,19 +12,21 @@ function convertBTC(currency = 'USD', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
 
   spinner.start();
-  request(url, (error, response, body) => {
-    let apiResponse;
-    spinner.stop();
 
-    try {
-      apiResponse = JSON.parse(body);
-    } catch (parseError) {
-      console.log(chalk.red('Something wen wrong in the API. Try in a few minutes.'));
-      return parseError;
-    }
-    console.log(`${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`);
-    return true;
-  });
+  return request(url)
+    .then((body) => {
+      spinner.stop();
+      return body;
+    })
+    .then((body) => {
+      const apiResponse = JSON.parse(body);
+      console.info(`${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`);
+    })
+    .catch((err) => {
+      spinner.stop();
+      console.info(chalk.red('Something wen wrong in the API. Try in a few minutes.'));
+      return err;
+    });
 }
 
 module.exports = convertBTC;
